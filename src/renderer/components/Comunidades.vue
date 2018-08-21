f97254fa673b6ebb665433f1eee2104a14c231b3
Yubisel <yubiselv@gmail.com>
2018-08-21 15:54:58 -0400

    montado ejemplo de resaltado por criterio de busqueda

--- src/renderer/components/Comunidades.vue
+++ src/renderer/components/Comunidades.vue
@@ -4,15 +4,41 @@
 
         Comunidades de recolectores-cazadores-pescadores reportados en la provincia de Granma
  
-En la provincia se encuentran reportados 127 sitios arqueológicos pertenecientes a estas comunidades, situados en diferentes puntos de la geografía, generalmente próximos a zonas cenagosas, ríos, arroyos y las costas. Se encuentran distribuidos de la siguiente manera por municipios:
+En la provincia se encuentran reportados 127 sitios arqueológicos pertenecientes a estas comunidades, 
+situados en diferentes puntos de la geografía, generalmente próximos a zonas cenagosas, ríos, 
+arroyos y las costas. Se encuentran distribuidos de la siguiente manera por municipios:
+
+        <div>
+            Search: <input type="text" v-model="query" placeholder="Search" />
+        </div>
+        <br />
+        <div v-html="highlight()"></div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                query: '',
+                content: 'este es un datos de prueba',
+            }
+        },
+        methods: {
+            highlight() {
+                if(!this.query) {
+                    return this.content;
+                }
+                return this.content.replace(new RegExp(this.query, "gi"), match => {
+                    return '<span class="highlightText">' + match + '</span>';
+                });
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style>
+    .highlightText {
+        background: yellow;
+    }
 </style>
