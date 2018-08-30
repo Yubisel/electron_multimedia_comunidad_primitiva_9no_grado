fb6426024a8d0e25b9bc7a129df7d1d16e187630
Yubisel <yubiselv@gmail.com>
2018-08-30 11:15:11 -0400

    cambio en el nombre

--- src/renderer/config/config.js
+++ src/renderer/config/config.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 
-Vue.prototype.$appName = 'Comunidad Primitiva 5to Grado'
+Vue.prototype.$appName = 'Comunidad Primitiva 9no Grado'
 Vue.prototype.$appFooter = 'Multimedia de Apoyo a la Enseñanza de la Historia de Cuba. Granma. Cuba 2018'
